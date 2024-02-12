a8c60e14f7d457f52eb0542b82d318e587520108
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-02-12 11:24:02 -0300

    feat: - User authentication completed - Middleware configured, receiving the verification token - Created error api to handle error exceptions - Configured typeorm for creating tables

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
-// import { UserController } from "./controllers/UserController";
-// import { authMiddleware } from "./middlewares/authMiddleware";
+import { UserController } from "./controllers/UserController";
+import { authMiddleware } from "./middleware/authMiddleware";
 
 const routes = Router();
 
-// routes.post("/user", new UserController().create);
-// routes.post("/login", new UserController().login);
+routes.post("/user", new UserController().create);
+routes.post("/login", new UserController().login);
 
-// routes.use(authMiddleware);
+routes.use(authMiddleware);
 
-// routes.get("/profile", new UserController().getProfile);
+routes.get("/profile", new UserController().getProfile);
 
 export default routes;
