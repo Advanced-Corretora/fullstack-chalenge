fb9ca5a03dad2f8563c0ee2b6db72f850068f744
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-02-13 11:18:42 -0300

    feat: - user auth settings

---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1>Voce esta logado! ✅</h1>
+      </div>
+    </>
+  );
+}
